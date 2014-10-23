9e95ce015376308fe3787a71d9dbd75d5c0db6f5
ssunkari <sr.sunkari@gmail.com>
2014-10-23 21:24:07 +0100

    Initial Nancy Setup

---
+++ application.Client/IndexModule.cs
@@ -0,0 +1,20 @@
+﻿namespace application.Client
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+
+            Get["/data"] = parameters =>
+            {
+                return "Helolo";
+            };
+        }
+    }
+}
